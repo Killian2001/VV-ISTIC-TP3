e4770e3c3fc4f767d6964a7944f98e8bc3252edf
Killian2001 <killianc2001@gmail.com>
2024-12-05 10:12:05 +0100

    implemented isBalanced

--- code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
+++ code/tp3-balanced-strings/src/main/java/fr/istic/vv/StringUtils.java
@@ -1,11 +1,52 @@
 package fr.istic.vv;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class StringUtils {
 
     private StringUtils() {}
 
+    /**
+     * Indicates if a string is balanced, i.e. <code>{}</code>, <code>[]</code> and <code>()</code>
+     * delimiters match each other, and are properly imbricated.
+     * For example, the string :
+     * <pre>{[][]}({})</pre>
+     * is balanced, while the following strings :
+     * <pre>][</pre> <pre>([)]</pre> <pre>{</pre> and <pre>{(}{}</pre>
+     * are not.
+     *
+     * @param str The string to check
+     * @return <code>true</code> if <code>str</code> is balanced, <code>false</code> otherwise.
+     */
     public static boolean isBalanced(String str) {
-        return false;
+        Deque<Character> symbStack = new ArrayDeque<>();
+        String opening = "{[(";
+        String closing = "}])";
+
+        for (char chr : str.toCharArray()) {
+            int opIndex = opening.indexOf(chr);
+            int clIndex = closing.indexOf(chr);
+
+            // Opening character: push it on the stack.
+            if (opIndex != -1)
+                symbStack.push(chr);
+
+            // Closing character...
+            if (clIndex != -1) {
+                // ...if the stack is empty then we do not recognize the word.
+                if (symbStack.isEmpty())
+                    return false;
+
+                // ...if the char at the top of the stack does not match with the corresponding char
+                // we do not recognize the word.
+                if (symbStack.pop() != opening.charAt(clIndex))
+                    return false;
+            }
+        }
+
+        // word is recognized if the stack is empty.
+        return symbStack.isEmpty();
     }
 
 }
