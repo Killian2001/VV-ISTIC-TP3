01a3cb78fbaf93387a06cf042d109b9bb976518f
Killian2001 <killianc2001@gmail.com>
2024-12-06 12:10:09 +0100

    done unit test for balanced-strings

--- code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
+++ code/tp3-balanced-strings/src/test/java/fr/istic/vv/StringUtilsTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringUtilsTest {
 
+    /* test cases obtained from initial input partition */
+
     /**
      * Pass a null string to isBalanced, and ensure it returns true (by vacuity).
      */
@@ -47,11 +49,11 @@ class StringUtilsTest {
     }
 
     /**
-     * Pass a simple balanced string with characters to isBalanced, and ensure it returns true.
+     * Pass a simple balanced string with letters to isBalanced, and ensure it returns true.
      * We use parentheses "()" for this test.
      */
     @Test
-    @DisplayName("simple balanced - parentheses and chars")
+    @DisplayName("simple balanced - parentheses and letters")
     void testBalancedSimpleParenthesesAndChars() {
         assertTrue(isBalanced("()a"));
     }
@@ -67,11 +69,11 @@ class StringUtilsTest {
     }
 
     /**
-     * Pass a simple balanced string to isBalanced with characters, and ensure it returns true.
+     * Pass a simple balanced string to isBalanced with letters, and ensure it returns true.
      * We use brackets "[]" for this test.
      */
     @Test
-    @DisplayName("simple balanced - brackets and chars")
+    @DisplayName("simple balanced - brackets and letters")
     void testBalancedSimpleBracketsAndChars() {
         assertTrue(isBalanced("[azerty]"));
     }
@@ -87,11 +89,11 @@ class StringUtilsTest {
     }
 
     /**
-     * Pass a simple balanced string with characters to isBalanced, and ensure it returns true.
+     * Pass a simple balanced string with letters to isBalanced, and ensure it returns true.
      * We use braces "{}" for this test.
      */
     @Test
-    @DisplayName("simple balanced - braces and chars")
+    @DisplayName("simple balanced - braces and letters")
     void testBalancedSimpleBracesAndChars() {
         assertTrue(isBalanced("az{er}ty"));
     }
@@ -107,11 +109,11 @@ class StringUtilsTest {
     }
 
     /**
-     * Pass a balanced string to isBalanced, with only parentheses imbrications and characters,
+     * Pass a balanced string to isBalanced, with only parentheses imbrications and letters,
      * and ensure it returns true.
      */
     @Test
-    @DisplayName("balanced - only imbricated parentheses and chars")
+    @DisplayName("balanced - only imbricated parentheses and letters")
     void testBalancedImbricatedParenthesesChars() {
         assertTrue(isBalanced("(az(((er))))ty"));
     }
@@ -127,11 +129,11 @@ class StringUtilsTest {
     }
 
     /**
-     * Pass a balanced string to isBalanced, with only brackets imbrications and chars,
+     * Pass a balanced string to isBalanced, with only brackets imbrications and letters,
      * and ensure it returns true.
      */
     @Test
-    @DisplayName("balanced - only imbricated brackets and chars")
+    @DisplayName("balanced - only imbricated brackets and letters")
     void testBalancedImbricatedBracketsAndChars() {
         assertTrue(isBalanced("a[z[e[r[t]y]i]u]o"));
     }
@@ -147,11 +149,11 @@ class StringUtilsTest {
     }
 
     /**
-     * Pass a balanced string to isBalanced, with only braces imbrications and chars,
+     * Pass a balanced string to isBalanced, with only braces imbrications and letters,
      * and ensure it returns true.
      */
     @Test
-    @DisplayName("balanced - only imbricated braces and chars")
+    @DisplayName("balanced - only imbricated braces and letters")
     void testBalancedImbricatedBracesAndChars() {
         assertTrue(isBalanced("{azer{{{ty}}}}"));
     }
@@ -167,11 +169,11 @@ class StringUtilsTest {
     }
 
     /**
-     * Pass a balanced string to isBalanced, with various imbrications and chars,
+     * Pass a balanced string to isBalanced, with various imbrications and letters,
      * and ensure it returns true.
      */
     @Test
-    @DisplayName("balanced - various imbrications and chars")
+    @DisplayName("balanced - various imbrications and letters")
     void testBalancedImbricatedVariousAndChars() {
         assertTrue(isBalanced("azer({[ty[(x)]]uiop})"));
     }
@@ -187,14 +189,134 @@ class StringUtilsTest {
     }
 
     /**
-     * Pass a balanced string to isBalanced, with a chain of various imbrications and characters,
+     * Pass a balanced string to isBalanced, with a chain of various imbrications and letters,
      * and ensure it returns true.
      */
     @Test
-    @DisplayName("balanced - succesive imbrications and chars")
+    @DisplayName("balanced - succesive imbrications and letters")
     void testBalancedSuccessiveImbricationsAndChars() {
         assertTrue(isBalanced("()[a(ze)ty]{{uiop[()]}qsdf}[]{[gh()]}jkl{}mw"));
     }
 
+    /* test cases added after statement coverage */
 
+    /**
+     * Pass an unbalanced string to isBalanced, with a closing char without an opening char
+     * and ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - closing char with no opening char")
+    void testUnbalancedNoOpening() {
+        assertFalse(isBalanced(")"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced, with a closing char without an opening char with
+     * letters and it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - closing char with no opening char and letters")
+    void testUnbalancedNoOpeningAndChars() {
+        assertFalse(isBalanced("az)er"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced, with a closing char without an opening char
+     * and ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - opening char with no closing char ")
+    void testUnbalancedOpeningNoClosing() {
+        assertFalse(isBalanced("[("));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced, with a closing char without an opening char,
+     * with letters and ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - opening char with no closing char with letters")
+    void testUnbalancedOpeningNoClosingAndChar() {
+        assertFalse(isBalanced("[azre(t"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced, with a closing char with a wrong opening char
+     * and ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - closing char with wrong opening char ")
+    void testUnbalancedClosingWrongOpening() {
+        assertFalse(isBalanced("([))"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced, with a closing char with a wrong opening char
+     * with letters and ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - closing char with wrong opening char ")
+    void testUnbalancedClosingWrongOpeningAndChar() {
+        assertFalse(isBalanced("az([erty)ui)"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced, with a closing char without opening char and closing
+     * char with a wrong opening char and ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - closing char without opening char, closing char with wrong opening char ")
+    void testUnbalancedWithoutOpeningWrongOpening() {
+        assertFalse(isBalanced("(}]"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced, with a closing char without opening char and closing
+     * char with a wrong opening char with letters and ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - closing char without opening char, closing char with wrong opening char ")
+    void testUnbalancedWithoutOpeningWrongOpeningAndChars() {
+        assertFalse(isBalanced("a(zert}]y"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced with multiple imbrications, and
+     * ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - multiple imbrications")
+    void testUnbalancedMultipleImbrications() {
+        assertFalse(isBalanced("{{({)]]}}"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced with multiple imbrications, and
+     * ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - multiple imbrications with letters")
+    void testUnbalancedMultipleImbricationsAndChars() {
+        assertFalse(isBalanced("{az{er(ttt{)yuio]]}ppp}"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced with a series of imbrications, and
+     * ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - succesive imbrications")
+    void testUnbalancedSuccessiveImbrications() {
+        assertFalse(isBalanced("{{()}}()[{}][(]}(]"));
+    }
+
+    /**
+     * Pass an unbalanced string to isBalanced with a series of imbrications with letters, and
+     * ensure it returns false.
+     */
+    @Test
+    @DisplayName("unbalanced - succesive imbrications with letters")
+    void testUnbalancedSuccessiveImbricationsAndChars() {
+        assertFalse(isBalanced("azerr{yuio{()}sdgfh}()[f{}]fvbdfn[(]f}(]"));
+    }
 }
