7908eaa702af56cf641a96315fcb9a9f67227388
Killian2001 <killianc2001@gmail.com>
2024-12-07 01:25:01 +0100

    added tests for nextDate and previousDate

--- code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
+++ code/tp3-date/src/test/java/fr/istic/vv/DateTest.java
@@ -58,7 +58,245 @@ class DateTest {
 
     /* tests for nextDate */
 
+    // input space partitioning tests
+
+    /**
+     * Create the 14/04/2001 date, get the next date, and ensure its the 15/04/2001.
+     */
+    @Test
+    @DisplayName("nextDate - day in the middle of a month of non-leap year")
+    void testNextDateMiddleMonthNonLeap() {
+        Date next = new Date(14, 4, 2001).nextDate();
+
+        assertEquals(15, next.getDay());
+        assertEquals(4, next.getMonth());
+        assertEquals(2001, next.getYear());
+    }
+
+    /**
+     * Create the 25/12/2001 date, get the next date, and ensure it is the 25/12/2001.
+     */
+    @Test
+    @DisplayName("nextDate - day in the middle of december of non-leap year")
+    void testNextDateMiddleDecemberNonLeap() {
+        Date next = new Date(24, 12, 2001).nextDate();
+
+        assertEquals(25, next.getDay());
+        assertEquals(12, next.getMonth());
+        assertEquals(2001, next.getYear());
+    }
+
+    /**
+     * Create the 31/03/2001 date, get the next date, and ensure it is the 01/04/2001.
+     */
+    @Test
+    @DisplayName("nextDate - last day of march non-leap year")
+    void testNextDateLastDayMarchNonLeap() {
+        Date next = new Date(31, 3, 2001).nextDate();
+
+        assertEquals(1, next.getDay());
+        assertEquals(4, next.getMonth());
+        assertEquals(2001, next.getYear());
+    }
+
+    /**
+     * Create the 31/03/2001 date, get the next date, and ensure it is the 01/04/2001.
+     */
+    @Test
+    @DisplayName("nextDate - last day of non-leap year")
+    void testNextDateLastDayYearNonLeap() {
+        Date next = new Date(31, 12, 2001).nextDate();
+
+        assertEquals(1, next.getDay());
+        assertEquals(1, next.getMonth());
+        assertEquals(2002, next.getYear());
+    }
+
+    /**
+     * Create the 14/04/2012 date, get the next date, and ensure its the 15/04/2012.
+     */
+    @Test
+    @DisplayName("nextDate - day in the middle of a month of leap year")
+    void testNextDateMiddleMonthLeap() {
+        Date next = new Date(14, 4, 2012).nextDate();
+
+        assertEquals(15, next.getDay());
+        assertEquals(4, next.getMonth());
+        assertEquals(2012, next.getYear());
+    }
+
+    /**
+     * Create the 25/12/2012 date, get the next date, and ensure it is the 25/12/2012.
+     */
+    @Test
+    @DisplayName("nextDate - day in the middle of december of leap year")
+    void testNextDateMiddleDecemberLeap() {
+        Date next = new Date(24, 12, 2012).nextDate();
+
+        assertEquals(25, next.getDay());
+        assertEquals(12, next.getMonth());
+        assertEquals(2012, next.getYear());
+    }
+
+    /**
+     * Create the 31/03/2012 date, get the next date, and ensure it is the 01/04/2012.
+     */
+    @Test
+    @DisplayName("nextDate - last day of march leap year")
+    void testNextDateLastDayMarchLeap() {
+        Date next = new Date(31, 3, 2012).nextDate();
+
+        assertEquals(1, next.getDay());
+        assertEquals(4, next.getMonth());
+        assertEquals(2012, next.getYear());
+    }
+
+    /**
+     * Create the 31/03/2012 date, get the next date, and ensure it is the 01/04/2012.
+     */
+    @Test
+    @DisplayName("nextDate - last day of leap year")
+    void testNextDateLastDayYearLeap() {
+        Date next = new Date(31, 12, 2012).nextDate();
+
+        assertEquals(1, next.getDay());
+        assertEquals(1, next.getMonth());
+        assertEquals(2013, next.getYear());
+    }
+
+    /**
+     * Create the 28/02/2012 date, get the next date, and ensure it is the 29/02/2012.
+     */
+    @Test
+    @DisplayName("nextDate - 28 february leap year")
+    void testNextDateLast28FebruaryLeap() {
+        Date next = new Date(28, 2, 2012).nextDate();
+
+        assertEquals(29, next.getDay());
+        assertEquals(2, next.getMonth());
+        assertEquals(2012, next.getYear());
+    }
+
     /* tests for previousDate */
+
+    // input space partitioning tests
+
+    /**
+     * Create the 14/04/2001 date, get the previous date, and ensure its the 13/04/2001.
+     */
+    @Test
+    @DisplayName("previousDate - day in the middle of a month of non-leap year")
+    void testPreviousDateMiddleMonthNonLeap() {
+        Date previous = new Date(14, 4, 2001).previousDate();
+
+        assertEquals(13, previous.getDay());
+        assertEquals(4, previous.getMonth());
+        assertEquals(2001, previous.getYear());
+    }
+
+    /**
+     * Create the 24/12/2001 date, get the previous date, and ensure it is the 23/12/2001.
+     */
+    @Test
+    @DisplayName("previousDate - day in the middle of december of non-leap year")
+    void testPreviousDateMiddleDecemberNonLeap() {
+        Date previous = new Date(24, 12, 2001).previousDate();
+
+        assertEquals(23, previous.getDay());
+        assertEquals(12, previous.getMonth());
+        assertEquals(2001, previous.getYear());
+    }
+
+    /**
+     * Create the 01/04/2001 date, get the previous date, and ensure it is the 31/03/2001.
+     */
+    @Test
+    @DisplayName("previousDate - first day of april non-leap year")
+    void testPreviousDateLastDayMarchNonLeap() {
+        Date previous = new Date(1, 4, 2001).previousDate();
+
+        assertEquals(31, previous.getDay());
+        assertEquals(3, previous.getMonth());
+        assertEquals(2001, previous.getYear());
+    }
+
+    /**
+     * Create the 01/01/2002 date, get the previous date, and ensure it is the 31/12/2001.
+     */
+    @Test
+    @DisplayName("previousDate - first day of non-leap year")
+    void testPreviousDateLastDayYearNonLeap() {
+        Date previous = new Date(1, 1, 2002).previousDate();
+
+        assertEquals(31, previous.getDay());
+        assertEquals(12, previous.getMonth());
+        assertEquals(2001, previous.getYear());
+    }
+
+    /**
+     * Create the 14/04/2012 date, get the previous date, and ensure it's the 13/04/2012.
+     */
+    @Test
+    @DisplayName("previousDate - day in the middle of a month of leap year")
+    void testPreviousDateMiddleMonthLeap() {
+        Date previous = new Date(14, 4, 2012).previousDate();
+
+        assertEquals(13, previous.getDay());
+        assertEquals(4, previous.getMonth());
+        assertEquals(2012, previous.getYear());
+    }
+
+    /**
+     * Create the 24/12/2012 date, get the previous date, and ensure it is the 23/12/2012.
+     */
+    @Test
+    @DisplayName("previousDate - day in the middle of december of leap year")
+    void testPreviousDateMiddleDecemberLeap() {
+        Date previous = new Date(24, 12, 2012).previousDate();
+
+        assertEquals(23, previous.getDay());
+        assertEquals(12, previous.getMonth());
+        assertEquals(2012, previous.getYear());
+    }
+
+    /**
+     * Create the 01/04/2012 date, get the previous date, and ensure it is the 31/03/2012.
+     */
+    @Test
+    @DisplayName("previousDate - first day of april leap year")
+    void testPreviousDateLastDayMarchLeap() {
+        Date previous = new Date(1, 4, 2012).previousDate();
+
+        assertEquals(31, previous.getDay());
+        assertEquals(3, previous.getMonth());
+        assertEquals(2012, previous.getYear());
+    }
+
+    /**
+     * Create the 01/01/2012 date, get the previous date, and ensure it is the 31/12/2011.
+     */
+    @Test
+    @DisplayName("previousDate - first day of leap year")
+    void testPreviousDateLastDayYearLeap() {
+        Date previous = new Date(1, 1, 2012).previousDate();
+
+        assertEquals(31, previous.getDay());
+        assertEquals(12, previous.getMonth());
+        assertEquals(2011, previous.getYear());
+    }
+
+    /**
+     * Create the 01/03/2012 date, get the next date, and ensure it is the 29/02/2012.
+     */
+    @Test
+    @DisplayName("previousDate - 28 february leap year")
+    void testPreviousDateLast28FebruaryLeap() {
+        Date previous = new Date(1, 3, 2012).previousDate();
+
+        assertEquals(29, previous.getDay());
+        assertEquals(2, previous.getMonth());
+        assertEquals(2012, previous.getYear());
+    }
 
     /* tests for isValidDate */
 
